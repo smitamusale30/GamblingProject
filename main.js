336a4fe7fad202afe79dfd50542bce2b7702ad11
smitamusale30 <smitamusale30@gmail.com>
2021-01-15 17:56:33 +0530

    update;finded the Luckiest and Unluckiest day

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 // Gambling Project Using Js
 /*
-UC1: As a Gambler, would start with a stake of $100 every day and bet $1 every game.
-
-UC2: As a Gambler make $1 bet so either win or loose $1
-
-UC3: As a Calculative Gambler if won or lost 50% of the stake, would resign for the day
-
-UC4: After 20 days of playing every day would like to know the total amount won or lost.
-
-UC5: Each month would like to know the days won and lost and by how much.
-
-UC6: Would also like to know my luckiest day where I won maximum and my unluckiest day where I lost maximum
-
 UC7: If won would like to continue playing next month or stop Gambling
 */
 function getRandomResult()
 {
     let arrMaxWonDays=new Array();
     let arrMaxLostDays=new Array(); 
+
+    let arrMaxWon=new Array();
+    let arrMaxLost=new Array(); 
 
     let arrDays=new Array();
     let arrAmount=new Array();
@@ -27,23 +18,43 @@ function getRandomResult()
     {
         console.log("Day: "+i);
         let winAmount=0,lostAmount=0;
+         //UC1: As a Gambler, would start with a stake of $100 every day and bet $1 every game.
         let amount=100;
-        const bet=1;
+        const BET=1;
         let min=amount/2;
+        let totalInteration=0,wonInteration=0,lostInteration=0;
+
+         //UC3: As a Ca lculative Gambler if won or lost 50% of the stake, would resign for the day
         while(winAmount<min && lostAmount<min)
         {
+            totalInteration++;
+            //UC2: As a Gambler make $1 bet so either win or loose $1
+            
             if (Math.random() < 0.5)
             {
-                amount=amount+bet;
+                wonInteration++;
+                amount=amount+BET;
                 winAmount++; 
             } 
             else       
             {    
-                amount=amount-bet;;       
+                lostInteration++;
+                amount=amount-BET;;       
                 lostAmount++;  
             } 
         }
+        //UC5: Each month would like to know the days won and lost and by how much.
         console.log("winamount:  "+winAmount+" lostAmount: "+lostAmount+" Amount: "+amount);
+        let lPersent=(wonInteration/totalInteration)*100;
+        let unlPersent=(lostInteration/totalInteration)*100;
+        if(lPersent>unlPersent)
+        {
+            arrMaxWonDays.push(lPersent);
+        }
+        else
+        {
+            arrMaxLostDays.push(unlPersent);
+        }
         if(winAmount>lostAmount)
         {
                 arrDays.push("win");
@@ -56,18 +67,6 @@ function getRandomResult()
         }
     }
 
-    for(let i=0;i<arrAmount.length;i++)
-    {
-        if(arrDays[i]=="win" && arrAmount[i]==50)
-        {
-            arrMaxWonDays.push(i);
-        }
-        if(arrDays[i]=="lost" && arrAmount[i]==50)
-        {
-            arrMaxLostDays.push(i);
-        }
-
-    }
 
     let temp=1;
     let tempWonAmount=0,tempLostAmount=0,maxWon=0,maxLost=0;
@@ -90,29 +89,38 @@ function getRandomResult()
         console.log("Day : "+temp+" you "+arrDays[i]+" the Game!!! "); 
         temp++;
     }
+     // UC4: After 20 days of playing every day would like to know the total amount won or lost.
     console.log("Total won days: "+winDay+" and lose Days: "+lostDays +" in one Month");
     
     console.log("Total Amount you Won is: "+tempWonAmount+"  lost Amount:  "+tempLostAmount+"  in One Month");
 
-    console.log("Luckiest Day are ");
+    
+     //UC6: Would also like to know my luckiest day where I won maximum and my unluckiest day where I lost maximum
+    let tempDay=0,luckiestDay=0,unluckiestDay=0;
     for(let i=0;i<arrMaxWonDays.length; i++)
     {
-       console.log(arrMaxWonDays[i]);
+        tempDay=arrMaxWonDays[0];
+        if(arrMaxWonDays[i]>tempDay)
+        {
+            tempDay=arrMaxWonDays[i];
+            luckiestDay=i;
+        }
     }
+    console.log("Luckiest Day is:  "+luckiestDay);
 
-    console.log("UN-Luckiest Day are ");
     for(let i=0;i<arrMaxLostDays.length; i++)
     {
-       console.log(arrMaxLostDays[i]);
+        tempDay=arrMaxLostDays[0];
+        if(arrMaxLostDays[i]>tempDay)
+        {
+            tempDay=arrMaxLostDays[i];
+            unluckiestDay=i;
+        }
     }
+    console.log("Un-Luckiest Day is:  "+unluckiestDay);
 
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-       
-      readline.question('Would You Like to Continue The Game ? Y/N :  ', ans => {
-        if(ans=="Y" || ans=="y")
+    
+        if(luckiestDay>unluckiestDay)
         {
             getRandomResult(); 
         }
@@ -120,7 +128,6 @@ function getRandomResult()
         {
             console.log("Thank You!!!! Visit Again");
         }
-        readline.close();
-      });
+       
 }
 getRandomResult(); 
